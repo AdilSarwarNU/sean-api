3c7905f99fc7c59cfa629482df9e0774b28743ab
Sam-Mauris Yong <mauris@hotmail.sg>
2016-10-06 11:51:26 +0800

    added error testing

--- test/index.js
+++ test/index.js
@@ -25,3 +25,31 @@ describe('Welcome', () => {
     });
   });
 });
+
+describe('Errors', () => {
+  describe('/GET JSON 404 route', () => {
+    it('it should return error message', (done) => {
+      chai.request(serverInstance)
+        .get(API_PREFIX + '/404')
+        .set('Accept', 'application/json')
+        .end((err, res) => {
+          res.should.have.status(404);
+          res.body.should.be.a('object');
+          res.body.should.have.property('error');
+          done();
+        });
+    });
+  });
+  
+  describe('/GET HTML 406 route', () => {
+    it('it should return error message', (done) => {
+      chai.request(serverInstance)
+        .get(API_PREFIX + '/404')
+        .set('Accept', 'text/html')
+        .end((err, res) => {
+          res.should.have.status(406);
+          done();
+        });
+    });
+  });
+});
